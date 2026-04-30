168d7e69d3bfa7a6f4ce7fb11b9d6b46fb88aa2f
Kevin Manoti <kevinnoty21@gmail.com>
2026-04-30 16:30:40 +0300

    Wire Up URLs

--- payments/urls.py
+++ payments/urls.py
@@ -1,3 +1,19 @@
 from django.urls import path
+from .views import (
+    InitiatePaymentView,
+    PaymentDetailView,
+    PaymentListView,
+)
 
-urlpatterns = []
+app_name = 'payments'
+
+urlpatterns = [
+    # Initiate a new payment
+    path('payments/initiate/', InitiatePaymentView.as_view(), name='initiate'),
+
+    # List all payments (with optional filters)
+    path('payments/', PaymentListView.as_view(), name='list'),
+
+    # Get specific payment by internal reference (UUID)
+    path('payments/<uuid:reference>/', PaymentDetailView.as_view(), name='detail'),
+]
